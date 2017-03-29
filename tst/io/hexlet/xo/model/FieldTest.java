419e0f570721f948f03a0867f9a31a927e960c27
MBosha <boshyn@gmail.com>
2017-03-30 00:25:42 +0300

    exception continue

--- tst/io/hexlet/xo/model/FieldTest.java
+++ tst/io/hexlet/xo/model/FieldTest.java
@@ -1,10 +1,13 @@
 package io.hexlet.xo.model;
 
+import io.hexlet.xo.model.exception.InvalidPointException;
 import org.junit.Test;
 
 import java.awt.*;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.fail;
 
 
 public class FieldTest {
@@ -18,11 +21,6 @@ public class FieldTest {
     }
 
     @Test
-    public void getFigure() throws Exception {
-
-    }
-
-    @Test
     public void setFigure() throws Exception {
 
         final Field field = new Field();
@@ -32,14 +30,31 @@ public class FieldTest {
         field.setFigure(inputPoint, inputFigure);
         final Figure actualFigure = field.getFigure(inputPoint);
 
-
         assertEquals(inputFigure, actualFigure);
+    }
 
+    @Test
+    public void getFigureWhenFigureIsNotSet() throws Exception {
 
+        final Field field = new Field();
+        final Point inputPoint = new Point(0,0);
 
+        final Figure actualFigure = field.getFigure(inputPoint);
 
+        assertNull(actualFigure);
+    }
 
+    @Test
+    public void getFigureWhenXIsLessThenZero() throws Exception {
 
+        final Field field = new Field();
+        final Point inputPoint = new Point(-1,0);
+
+        try {
+
+            field.getFigure(inputPoint);
+            fail();
+        } catch (final InvalidPointException e) {}
     }
 
 }
