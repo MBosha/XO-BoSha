0d4b05b847d86369b7eeb2b39ce26adc2febe660
MBosha <boshyn@gmail.com>
2017-03-29 23:05:01 +0300

    create class testPlayer

---
+++ tst/io/hexlet/xo/model/PlayerTest.java
@@ -0,0 +1,35 @@
+package io.hexlet.xo.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PlayerTest {
+    @Test
+    public void getName() throws Exception {
+
+        final String inputValue = "Bosha";
+        final String expectedValue = inputValue;
+
+        final Player player = new Player(inputValue, null);
+
+        final String actualValue = player.getName();
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void getFigure() throws Exception {
+
+        final Figure inputValue = Figure.X;
+        final Figure expectedValue = inputValue;
+
+        final Player player = new Player(null, inputValue);
+
+        final Figure actualValue = player.getFigure();
+
+        assertEquals(expectedValue, actualValue);
+
+    }
+
+}
