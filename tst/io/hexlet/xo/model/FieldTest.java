948c2907088d5bdae860abb4e16605b23692ef42
MBosha <boshyn@gmail.com>
2017-03-30 00:44:28 +0300

    exception continue 2

--- tst/io/hexlet/xo/model/FieldTest.java
+++ tst/io/hexlet/xo/model/FieldTest.java
@@ -1,5 +1,6 @@
 package io.hexlet.xo.model;
 
+import io.hexlet.xo.model.exception.AlreadyOccupiedException;
 import io.hexlet.xo.model.exception.InvalidPointException;
 import org.junit.Test;
 
@@ -94,6 +95,22 @@ public class FieldTest {
             field.getFigure(inputPoint);
             fail();
         } catch (final InvalidPointException e) {}
+    }
+
+    @Test
+    public void setFigureWhenAlreadyOccupited() throws Exception {
+
+        final Field field = new Field();
+        final Point inputPoint = new Point(0,0);
+        final  Figure inputFigure = Figure.O;
+
+        field.setFigure(inputPoint, inputFigure);
+        try {
+            field.setFigure(inputPoint, inputFigure);
+            fail();
+        } catch (final AlreadyOccupiedException e) {}
+
+
     }
 
 }
